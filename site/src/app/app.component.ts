57df94fb1911f2e99c25e38a367f36e7ebf2be70
brocktubre <brocktubre@gmail.com>
2019-07-25 20:01:48 -0400

    Added info about K8

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Docker + Angular + Ansible';
+  title = 'Kubernetes + Docker + Angular + Ansible';
   date = new Date().getFullYear();
 }
